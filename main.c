b32148951d5aca7ea0417dd69f3406aebd501b66
Danielle <danielle.w.raine@outlook.com>
2024-06-09 01:51:18 -0500

    Started event polling, program now opens window, prints keys that are pressed, and can close! Some comments changed.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 
@@ -15,39 +16,41 @@ int main(void)
 	printf("Homepage Url: %s\n", ManySnakes_HOMEPAGE_URL);
 	
 	// print game version
-	printf("Game Version: "); 
-	
+	printf("Game Version: ");
 	switch (ManySnakes_VERSION_PHASE)
 	{
-		case 0:
+		case 0: // is release version
 			printf("Release %d.%d.%d\n", ManySnakes_VERSION_MAJOR, ManySnakes_VERSION_MINOR, ManySnakes_VERSION_PATCH);
 			break;
-		case 1:
+		case 1: // is release preview version
 			printf("Release %d.%d.0-preview.%d\n", ManySnakes_VERSION_MAJOR, ManySnakes_VERSION_MINOR, ManySnakes_VERSION_PATCH);
 			break;
-		case 2:
+		case 2: // is pre-alpha version
 			printf("pre-alpha.%d\n", ManySnakes_VERSION_PATCH);
 			break;
-		case 3:
+		case 3: // is alpha version
 			printf("alpha-%d.%d\n", ManySnakes_VERSION_MINOR, ManySnakes_VERSION_PATCH);
 			break;
-		case 4:
+		case 4: // is beta version
 			printf("beta-%d.%d.%d\n", ManySnakes_VERSION_MAJOR, ManySnakes_VERSION_MINOR, ManySnakes_VERSION_PATCH);
 			break;
-		default:
+		default: // what happened here???
 			printf("Unknown???\n");
 	}
 
-	//printf("Game Version: %d.%d.%d", ManySnakes_VERSION_MAJOR, ManySnakes_VERSION_MINOR, ManySnakes_VERSION_PATCH);
+	// print c version
 	printf("C Version: %d\n", __STDC_VERSION__ );
+	
 	// sdl version structs
 	SDL_version compiled;
 	SDL_version linked;
+	
 	// print compiled, linked sdl version
 	SDL_VERSION(&compiled);
 	SDL_GetVersion(&linked);
 	SDL_Log("Compiled against SDL version %u.%u.%u\n", compiled.major, compiled.minor, compiled.patch);
 	SDL_Log("Linking against SDL version %u.%u.%u\n", linked.major, linked.minor, linked.patch);
+
 
 	// register sdl shutdown on program closure
 	atexit(SDL_Quit);
@@ -55,12 +58,49 @@ int main(void)
 	// initialize sdl. if error, return 
 	if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_EVENTS) != 0)
 	{
-		printf("%s", SDL_GetError());
+		printf("ERROR: %s", SDL_GetError());
 		return 1;
 	}
 
+
+	// create window
+	SDL_Window *window = SDL_CreateWindow("ManySnakes", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_RESIZABLE);
+	// if window doesn't exist, print error and return
+	if (!window)
+	{
+		printf("ERROR: %s", SDL_GetError());
+		return 1;
+	}
+
+	SDL_Event event;
+	bool isRunning = true;
+	// main loop	
+	while (isRunning)
+	{
+		// poll until all events handled
+		while(SDL_PollEvent(&event))
+		{
+			if (SDL_QUIT == event.type) // if quit, stop event poll and exit main loop
+			{
+				isRunning = false;
+				break;
+			}
+			else if (SDL_KEYDOWN == event.type) // if key pressed down, handle it!
+			{
+				SDL_Log("%s", SDL_GetKeyName(event.key.keysym.sym));
+			}
+		}
+
+		// update game state, draw current frame
+
+	} // main loop end
+
+
+	// desstroy window
+	SDL_DestroyWindow(window);
+	
 	// print sdl subsystem initialization flags
-	printf("%X\n\n", SDL_WasInit(SDL_INIT_EVERYTHING));
+	// printf("%X\n\n", SDL_WasInit(SDL_INIT_EVERYTHING));
 
 	return 0;
 }
