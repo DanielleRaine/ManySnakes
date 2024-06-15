3ace1ffc8491df1c4c4c68cc783890a5a5f04d7d
Danielle Raine <danielle.w.raine@outlook.com>
2024-06-15 02:19:41 -0500

    Implemented the snake and a set number of frames per second.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* ManySnakes by Danielle Raine
+/* ManyS  nakes by Danielle Raine
  * Start Date: June 8th, 2024
  */
 
@@ -12,6 +12,14 @@
 
 int main(void)
 {
+	typedef struct SnakeNode
+	{
+		struct SnakeNode *next;
+		int xPos;
+		int yPos;
+	} SnakeNode;
+
+
 	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	 * Print the game's description, Github URL, and version numbers of the game, C, and SDL.
 	 */
@@ -44,7 +52,7 @@ int main(void)
 	}
 
 	// print c version
-	SDL_Log("C Version %d\n", __STDC_VERSION__ );
+	SDL_Log("C Version %ld\n", __STDC_VERSION__ );
 	
 	// sdl version structs
 	SDL_version compiled;
@@ -108,21 +116,53 @@ int main(void)
 		return 1;
 	}
 
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 * Create the player's snake and seed rand.
+	 */
+
+	SnakeNode *playerHead = malloc(sizeof(SnakeNode));
+	playerHead->next = NULL;
+	playerHead->xPos = WINDOW_WIDTH / 2;
+	playerHead->yPos = WINDOW_HEIGHT / 2;
+
+	SnakeNode *playerCurNode = playerHead;
+
+	for (int i = 0; i < 2; i++)
+	{
+		int xPosNext = playerCurNode->xPos;
+		int yPosNext = playerCurNode->yPos + 24;
+
+		playerCurNode->next = malloc(sizeof(SnakeNode));
+		playerCurNode = playerCurNode->next;
+		playerCurNode->next = NULL;
+		playerCurNode->xPos = xPosNext;
+		playerCurNode->yPos = yPosNext;
+	}
+	
+	srand(SDL_GetTicks());
 
 	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	 * Main loop, poll for events!
 	 */
-	
-	srand(SDL_GetTicks());
-	SDL_RenderClear(renderer);
+
+	char playerDirection = 'u';
+	//Uint64 timeLastMove = SDL_GetTicks64();
+	Uint64 timeNextMove = SDL_GetTicks64() + 500;
 
 	SDL_Event event;
 	bool isRunning = true;
 	// main loop	
 	while (isRunning)
 	{
+		// clear frame
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderClear(renderer);
+
+		// set the earliest time the next frame occurs
+		Uint64 timeNextFrame = SDL_GetTicks64() + (1000 / 60);
+		
 		// poll events
-		while(SDL_PollEvent(&event))
+		while (SDL_PollEvent(&event))
 		{
 			if (SDL_QUIT == event.type) // if quit, stop event poll and exit main loop
 			{
@@ -131,35 +171,135 @@ int main(void)
 			}
 			else if (SDL_KEYDOWN == event.type) // if key pressed down, handle it!
 			{
-				// log the name of pressed key
-				SDL_Log("%s", SDL_GetKeyName(event.key.keysym.sym));
+				SDL_Keycode pressedKey = event.key.keysym.sym;
 
-				if (event.key.keysym.sym == SDLK_f) // key f
+				// log the name of pressed key
+				SDL_Log("%s", SDL_GetKeyName(pressedKey));
+
+				if (pressedKey == SDLK_RIGHT && playerDirection != 'l') // pressed right key, skip if direction is left
 				{
-					// draw a random box on a snake space
-					SDL_Rect drawRect = {rand() % 40 * WINDOW_WIDTH / 40, rand() % 30 * WINDOW_HEIGHT / 30, WINDOW_WIDTH / 40, WINDOW_HEIGHT / 30};
-					SDL_Log("Rect at %d, %d with %d, %d.", drawRect.x, drawRect.y, drawRect.w, drawRect.h);
-					if (SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255) || SDL_RenderFillRect(renderer, &drawRect))
-					{
-						SDL_LogError(SDL_LOG_CATEGORY_ERROR, "%s", SDL_GetError());
-						SDL_DestroyRenderer(renderer);
-						SDL_DestroyWindow(window);
-						return 1;
-					}
+					playerDirection = 'r'; // right
 				}
+				else if (pressedKey == SDLK_UP && playerDirection != 'd') // pressed up key, skip if direction is down
+				{
+					playerDirection = 'u'; // up
+				}
+				else if (pressedKey == SDLK_LEFT && playerDirection != 'r') // pressed left key, skip if direction is right
+				{
+					playerDirection = 'l'; // left
+				}
+				else if (pressedKey == SDLK_DOWN && playerDirection != 'u') // pressed down key, skip if direction is up
+				{
+					playerDirection = 'd'; // down
+				}
+				//else if (event.key.keysym.sym == SDLK_f) // key f
+				//{
+				//	// draw a random box on a snake space
+				//	SDL_Rect drawRect = {rand() % 40 * WINDOW_WIDTH / 40, rand() % 30 * WINDOW_HEIGHT / 30, WINDOW_WIDTH / 40, WINDOW_HEIGHT / 30};
+				//	SDL_Log("Rect at %d, %d with %d, %d.", drawRect.x, drawRect.y, drawRect.w, drawRect.h);
+				//	if (SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255) || SDL_RenderFillRect(renderer, &drawRect))
+				//	{
+				//		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "%s", SDL_GetError());
+				//		SDL_DestroyRenderer(renderer);
+				//		SDL_DestroyWindow(window);
+				//		return 1;
+				//	
+				//	}
+				//}
 			}
 
 		}
+
+		/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * Player's snake moves every 1/2 of a second, so if current time is greater than or equal to the next
+		 * time the snake is supposed to move, update the position of the snake depending on the direction 
+		 * that the player pressed.
+		 */
+
+		Uint64 timeNow = SDL_GetTicks64();
+
+		if (timeNow >= timeNextMove)
+		{
+			timeNextMove = timeNow + 500;
+
+			int xPosDelta, yPosDelta;
+
+			SDL_Log("%d\n", playerDirection);
+			
+			switch (playerDirection)
+			{
+
+				case 'r':
+					xPosDelta = WINDOW_WIDTH / 40;
+					yPosDelta = 0;
+					break;
+				case 'u':
+					xPosDelta = 0;
+					yPosDelta = -(WINDOW_HEIGHT / 30);
+					break;
+				case 'l':
+					xPosDelta = -(WINDOW_WIDTH / 40);
+					yPosDelta = 0;
+					break;
+				case 'd':
+					xPosDelta = 0;
+					yPosDelta = WINDOW_HEIGHT / 30;
+					break;
+				default:
+					SDL_Log("Huh??");
+			}
+
+			playerCurNode = playerHead;
+			int xPosNext = playerCurNode->xPos + xPosDelta;
+			int yPosNext = playerCurNode->yPos + yPosDelta;
+			while (playerCurNode)
+			{
+				int xPosOld = playerCurNode->xPos;
+				int yPosOld = playerCurNode->yPos;
+				playerCurNode->xPos = xPosNext;
+				playerCurNode->yPos = yPosNext;
+				xPosNext = xPosOld;
+				yPosNext = yPosOld;
+				playerCurNode = playerCurNode->next;
+			}
+		}
+
+		/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * Draw the snake and wait, wait until next frame, draw frame.
+		 */
+		
+		playerCurNode = playerHead;
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		while (playerCurNode)
+		{
+			// SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255);
+			SDL_Rect drawRect = {playerCurNode->xPos, playerCurNode->yPos, WINDOW_WIDTH / 40, WINDOW_HEIGHT / 30};
+			SDL_RenderFillRect(renderer, &drawRect);
+			playerCurNode = playerCurNode->next;
+		}
+		
+		// wait until next frame
+		while (SDL_GetTicks64() < timeNextFrame);
 		
 		// update game state, draw current frame
 		SDL_RenderPresent(renderer);
 		
-		// delay 0.1 seconds.
-		SDL_Delay(100);
 	} // main loop end
 
 
-	// cleanup
+	
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 * Free the player's snake's nodes and destroy renderer, window.
+	 */
+
+	playerCurNode = playerHead;
+	while (playerCurNode)
+	{
+		 SnakeNode *playerNextNode = playerCurNode->next;
+		 free(playerCurNode);
+		 playerCurNode = playerNextNode;
+	}	
+
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
